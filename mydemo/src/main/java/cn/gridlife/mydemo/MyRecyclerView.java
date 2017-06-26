790ea963847e645e19055f209b44420ee5d93431
xiaobei56 <xiaobei_63@163.com>
2017-06-26 18:28:16 +0800

    添加model

---
+++ mydemo/src/main/java/cn/gridlife/mydemo/MyRecyclerView.java
@@ -0,0 +1,18 @@
+package cn.gridlife.mydemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * cn.gridlife.mydemo
+ * TeleText
+ * Created by BEI on 2017/6/26.
+ */
+
+public class MyRecyclerView extends Activity {
+	@Override
+	protected void onCreate( Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.recycler_view_layout);
+	}
+}
