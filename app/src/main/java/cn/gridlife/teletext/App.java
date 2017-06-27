21cdc93f98b142ab650546b92a1c8e6f80254352
xiaobei56 <xiaobei_63@163.com>
2017-06-27 18:05:13 +0800

    添加 leakcanary

--- app/src/main/java/cn/gridlife/teletext/App.java
+++ app/src/main/java/cn/gridlife/teletext/App.java
@@ -2,6 +2,8 @@ package cn.gridlife.teletext;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * cn.gridlife.teletext
  * TeleText
@@ -9,4 +11,14 @@ import android.app.Application;
  */
 
 public class App extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		if (LeakCanary.isInAnalyzerProcess(this)) {
+			// This process is dedicated to LeakCanary for heap analysis.
+			// You should not init your app in this process.
+			return;
+		}
+		LeakCanary.install(this);
+	}
 }
